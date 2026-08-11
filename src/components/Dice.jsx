2adf11f7d08240cb4b9ab2b11e28930a158495f4
User-MiMa <mlsmasson@outlook.com>
2026-08-11 17:55:41 -0600

    Revert "merge: merge dev updated" due to wrong conflict handling
    This reverts commit 03278cca7826c077e950931e5be69fcc42fe1502.

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,16 +1,5 @@
 export default function Dice(props){
-<<<<<<< HEAD
     return(
         <button>{props.value}</button>
-=======
-    const styles={
-        backgroundColor: props.isHeld ? "#48ca15" : "#f0f0f0"
-    }
-    function triggerHoldDie(){
-        props.fn(props.id)
-    }
-    return(
-        <button onClick={triggerHoldDie} style={styles}>{props.value}</button>
->>>>>>> dev
     );
 }
