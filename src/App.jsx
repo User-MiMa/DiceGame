6d6afa9655d5b8683836c5668aa8965f73e136aa
User-MiMa <mlsmasson@outlook.com>
2026-08-11 13:24:32 -0600

    refactor: change arr state to object

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import Dice from "./components/Dice";
 import { useState } from "react";
+import { nanoid } from "nanoid";
+
 
 export default function App(){
 
     function getRandomDice(){
         const diceArray=[];
         for (let i=0;i<15;i++){
-            diceArray.push(Math.ceil(Math.random()*6))
+            diceArray.push(
+                {
+                    id: nanoid(),
+                    value: Math.ceil(Math.random()*6),
+                    isHeld: false
+                }
+            )
         }
         return diceArray;
     }
 
     const [dice, setDice] = useState(getRandomDice())
-    const diceElements = dice.map(num => <Dice value={num} />)
+    const diceElements = dice.map(die => <Dice id={die.id} value={die.value} isHeld={die.isHeld}/>)
 
     function rollDice(){
         setDice(getRandomDice());
