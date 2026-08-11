90df7415e7a16f9a107c629d46bf73253c49995f
User-MiMa <mlsmasson@outlook.com>
2026-08-11 00:12:18 -0600

    feat: add Dice component and styling to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import Dice from "./components/Dice";
+
 export default function App(){
     return (
-        <h1>Placeholder</h1>
+        <main>
+            <Dice
+            value={1}
+            />
+            <Dice
+            value={2}
+            />
+            <Dice
+            value={3}
+            />
+            <Dice
+            value={4}
+            />
+            <Dice
+            value={5}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+        </main>
     );
 }
