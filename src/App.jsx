ddc2c39e91b01ef90fb71336c74f0c88009c0cd0
User-MiMa <mlsmasson@outlook.com>
2026-08-11 00:13:05 -0600

    merge: merge dev branch

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Dice from "./components/Dice";
+
+export default function App(){
+    return (
+        <main>
+            <Dice
+            value={1}
+            />
+            <Dice
+            value={2}
+            />
+            <Dice
+            value={3}
+            />
+            <Dice
+            value={4}
+            />
+            <Dice
+            value={5}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+            <Dice
+            value={6}
+            />
+        </main>
+    );
+}
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: 'src/index.html'
-    }
+    outDir: '../dist'
   }
 })
